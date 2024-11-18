5578ff14b1f43169017a4056fbc225d7426a4ce3
RiccardoFiacco <riccardofiacco17@gmail.com>
2024-11-18 16:01:11 +0100

    add the images, and add a js file with five obj. Also imported the arr with obj in the app.js

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const express = require('express');
 const app = express();//creiamo un server
 const port = 3000; //andiam0o a dare un numero per la porta
 
+const array = require("./array.js");
+console.log(array)
 app.listen(port, () => {  //mettiamo il server in ascolto alla porta numero 3000
     console.log(`sono in ascolto sulla porta numero ${port}`)
 })
 
 app.get('/', (req, res) => { //alla richiesta  di tipo get verso la rotta url base, si rispondera con quello che è dentro il .send
     res.send("Server del mio blog")
-  })
+})
+
---
+++ array.js
@@ -0,0 +1,34 @@
+const array = [
+    {
+        titolo:"ciambellone",
+        contenuto:"ciambellone",
+        img:"./public/images/cimabellone.jpeg",
+        tags:["cia","mbel","lone"]
+    },
+    {
+        titolo:"cracker barbabietola",
+        contenuto:"cracker barbabietola",
+        img:"./public/images/cracker_barbabietola.jpeg",
+        tags:["cracker","barba","bietola"]
+    },
+    {
+        titolo:"pane fritto dolce",
+        contenuto:"pane fritto dolce",
+        img:"./public/images/pane_fritto_dolce.jpeg",
+        tags:["pane","fritto","dolce"]
+    },
+    {
+        titolo:"pasta barbabietola",
+        contenuto:"pasta barbabietola",
+        img:"./public/images/pasta_barbabietola.jpeg",
+        tags:["pasta","barba","bietola"]
+    },
+    {
+        titolo:"torta paesana",
+        contenuto:"torta paesana",
+        img:"./public/images/torta_paesana.jpeg",
+        tags:["torta","paes","ana"]
+    }
+]
+
+module.exports = {array}
