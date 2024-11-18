5e1020239c92472248eb6afe8b717dc4e7e3526e
RiccardoFiacco <riccardofiacco17@gmail.com>
2024-11-18 15:33:08 +0100

    add comments

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const app = express();
-const port = 3000;
+const app = express();//creiamo un server
+const port = 3000; //andiam0o a dare un numero per la porta
 
-app.listen(port, () => {
+app.listen(port, () => {  //mettiamo il server in ascolto alla porta numero 3000
     console.log(`sono in ascolto sulla porta numero ${port}`)
 })
 
-app.get('/', (req, res) => {
+app.get('/', (req, res) => { //alla richiesta  di tipo get verso la rotta url base, si rispondera con quello che è dentro il .send
     res.send("Server del mio blog")
   })
